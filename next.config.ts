e03509e77f5814108afdb17490dba3f6ed8afb3b
Adbuzz <adbuzz2023@gmail.com>
2025-06-11 14:03:50 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,9 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  experimental: {
+    allowedDevOrigins: ['https://6000-firebase-studio-1749633367356.cluster-lqnxvk7thvfw4wbonsercicksm.cloudworkstations.dev'],
+  },
 };
 
 export default nextConfig;
